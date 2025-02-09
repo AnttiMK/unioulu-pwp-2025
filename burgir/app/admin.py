6db1f9c151752ba1764f19fbb241be54219fb8e3
Markus Teuhola <mteuhola19@student.oulu.fi>
2025-02-09 20:33:02 +0200

    Edit OrderItems in Admin Panel

--- burgir/app/admin.py
+++ burgir/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Table, User, Reservation, MenuItem, Order
+from .models import Table, User, Reservation, MenuItem, Order, OrderItem
 
 # Register your models here.
 admin.site.register(Table)
@@ -7,3 +7,4 @@ admin.site.register(User)
 admin.site.register(Reservation)
 admin.site.register(MenuItem)
 admin.site.register(Order)
+admin.site.register(OrderItem)
